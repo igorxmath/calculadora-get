2048225413940fdbd88593fdaef41f21154db745
igorxmath <103374874+igorxmath@users.noreply.github.com>
2022-12-16 10:51:12 -0300

    add only number function

--- js/script.js
+++ js/script.js
@@ -47,3 +47,11 @@ function fixStepIndicator(n) {
     }
     x[n].className += " active" 
 }
+
+function forceInputNumber(evt) {
+    var ASCIICode = (evt.which) ? evt.which : evt.keyCode
+    if ASCIICode > 31 && (ASCIICode < 48 || ASCIICode > 57) {
+        return false;
+    }
+    return true;
+}
